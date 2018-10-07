c1e7bfcc8b88242723453612e1a465cd830a6d1e
Álvaro Torralba <donfrutosgomez@gmail.com>
2018-10-07 12:43:25 -0300

    Create read.c

---
+++ src/read.c
@@ -0,0 +1,31 @@
+/*
+ * read.c
+ * 
+ * Copyright 2018 Alvarito050506 <donfrutosgomez@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+
+char main(int argc, char* argv[])
+{
+	char scanvar[256];
+	printf("%s ", argv[1]);
+	scanf("%s", &scanvar);
+	return scanvar;
+}
