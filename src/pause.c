82fc315ed146149df25031ef8879cb0a66abcefd
Álvaro Torralba <donfrutosgomez@gmail.com>
2018-10-07 12:41:26 -0300

    Create pause.c

---
+++ src/pause.c
@@ -0,0 +1,35 @@
+/*
+ * pause.c
+ * 
+ * Copyright 2018 Alvarito050506 <donfrutosgomez@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	char scanvar;
+	printf("Presione cualquier tecla para continuar... ");
+	scanf("%c", &scanvar);
+	if(scanvar == NULL)
+	{
+		return 2;
+	}
+	return 0;
+}
